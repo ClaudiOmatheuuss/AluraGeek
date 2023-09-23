6365e77a1d73230d1372c8759e95bc903f87e324
Claud10matheus <claudiom.pst@gmail.com>
2023-09-22 21:36:41 -0300

    melhorando o verProduto

--- assets/js/controllers/verProduto.js
+++ assets/js/controllers/verProduto.js
@@ -1,7 +1,6 @@
 import { conectaApi } from '../services/produtos-services.js'
-import criaCard from './criaCard-controllers.js'
 
-async function carregaProdutos () {
+async function carregaProdutos() {
     const listaProdutos = await conectaApi.listaProdutos()
     //armazena lista de produstos da API
 
@@ -43,18 +42,35 @@ function carregaSimilares(produtoSelecionado, listaProdutos) {
     const similaresSection = document.querySelector(".similar__products");
 
     //filtra produtos da api com a mesma categoria em comum do produto selecionado
-    let produtosSimilares = listaProdutos.filter( item => item.section === produtoSelecionado.section )
+    let produtosSimilares = listaProdutos.filter(item => item.section === produtoSelecionado.section)
     //filtra entre os produtos da mesma categoria apenas os produtos com id diferente do produto selecionado
-    produtosSimilares = produtosSimilares.filter( item => item.id !== Number(produtoSelecionado.id) )
+    produtosSimilares = produtosSimilares.filter(item => item.id !== Number(produtoSelecionado.id))
 
     //recorta itens filtrados da mesma categoria do indice 0 até 5, ou seja, retorna 6 objetos de produtos
-    produtosSimilares = produtosSimilares.slice(0,5)
+    produtosSimilares = produtosSimilares.slice(0, 5)
 
     //para cada produto similar cria um card
-    produtosSimilares.forEach( product =>
-        similaresSection.appendChild(criaCard( product.name, product.id, product.imageUrl, product.price ))
+    produtosSimilares.forEach(product =>
+        similaresSection.appendChild(criaSimilares(product.name, product.id, product.imageUrl, product.price))
     )
 
+}
+
+function criaSimilares(name, id, imageUrl, price) {
+    const card = document.createElement("li")
+    card.className = `product-card`
+    const conteudo = `
+                        <a href="./produto.html?id=${id}">
+                            <img src=".${imageUrl}"
+                            alt="imagem do produto">
+                        </a>
+                        <p class="card-nome">${name}</p>
+                        <p class="card-preco">R$ ${price}</p>
+                        <a href="./produto.html?id=${id}" class="card-link">Ver produto</a>
+        `
+
+    card.innerHTML = conteudo
+    return card
 }
 
 carregaProdutos();
