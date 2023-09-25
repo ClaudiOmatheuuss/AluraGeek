9ccd2e4aab97c4c2e12576c419eb723c76f507ee
Claud10matheus <claudiom.pst@gmail.com>
2023-09-25 13:07:54 -0300

    resolvendo erros de escrita

--- assets/js/controllers/exibePesquisa-controllers.js
+++ assets/js/controllers/exibePesquisa-controllers.js
@@ -2,13 +2,13 @@ export default function cardProduto (name, id, imageUrl, price) {
     const card = document.createElement("li")
     card.className = `product-card` 
     const conteudo = `
-                    <a href="../views/produto.html?id=${id}">
+                    <a href="./views/produto.html?id=${id}">
                         <img src="${imageUrl}"
                         alt="imagem do produto">
                     </a>
                     <p class="card-nome">${name}</p>
                     <p class="card-preco">R$ ${price},99</p>
-                    <a href="../views/produto.html?id=${id}" class="card-link">Ver produto</a>
+                    <a href="./views/produto.html?id=${id}" class="card-link">Ver produto</a>
     `
 
     card.innerHTML = conteudo
